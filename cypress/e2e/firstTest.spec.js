273bda7c97cd1c951cd56e01dd1cfffa29bfea56
Julio Guerra <juliowar33@gmail.com>
2022-09-13 18:53:29 -0500

    Add tests

---
+++ cypress/e2e/firstTest.spec.js
@@ -0,0 +1,290 @@
+/// <reference types="cypress" />
+import "@testing-library/cypress/add-commands";
+
+describe("our first suite", () => {
+  it("first test", () => {
+    cy.visit("/");
+    cy.contains(/forms/i).click();
+    cy.contains(/form layouts/i).click();
+
+    //by Tag Name
+    cy.get("input");
+
+    //by Id
+    cy.get("#inputEmail1");
+
+    //by Class name
+    cy.get(".input-full-width");
+
+    //by Attribute name
+    cy.get("[placeholder]");
+
+    //by Attribute name and value
+    cy.get('[placeholder="Email"]');
+
+    //by Class value (should be complete)
+    cy.get('[class="input-full-width size-medium shape-rectangle"]');
+
+    //by Tag name and Attribute with value
+    cy.get('input[placeholder="Email"]');
+
+    //by two different attributes
+    cy.get('[placeholder="Email"][type="email"]');
+
+    //Most recommended way by Cypress
+    cy.get('[data-cy="inputEmail1"]');
+  });
+
+  it("second test", () => {
+    cy.visit("/");
+    cy.contains(/forms/i).click();
+    cy.contains(/form layouts/i).click();
+
+    // cy.findAllByText(/sign in/i)
+    //   .first()
+    //   .click();
+
+    cy.contains('[status="warning"]', /sign in/i);
+
+    cy.get("#inputEmail3")
+      .parents("form")
+      .find("button")
+      .should("contain", "Sign in")
+      .parents("form")
+      .find("nb-checkbox")
+      .click();
+
+    cy.contains("nb-card", "Horizontal form").find('[type="email"]');
+  });
+
+  it("then and wrap methods", () => {
+    cy.visit("/");
+    cy.contains(/forms/i).click();
+    cy.contains(/form layouts/i).click();
+
+    // cy.contains("nb-card", "Using the Grid")
+    //   .find('[for="inputEmail1"]')
+    //   .should("contain", "Email");
+
+    // cy.contains("nb-card", "Using the Grid")
+    //   .find('[for="inputPassword2"]')
+    //   .should("contain", "Password");
+
+    // cy.contains("nb-card", "Basic form")
+    //   .find('[for="exampleInputEmail1"]')
+    //   .should("contain", "Email address");
+
+    // cy.contains("nb-card", "Basic form")
+    //   .find('[for="exampleInputPassword1"]')
+    //   .should("contain", "Password");
+
+    cy.contains("nb-card", "Using the Grid").then((firstForm) => {
+      const emailLabelFirst = firstForm.find('[for="inputEmail1"]').text();
+      const passwordLabelFirst = firstForm
+        .find('[for="inputPassword2"]')
+        .text();
+
+      expect(emailLabelFirst).to.equal("Email");
+      expect(passwordLabelFirst).to.equal("Password");
+    });
+  });
+
+  it("invoke command", () => {
+    cy.visit("/");
+    cy.contains(/forms/i).click();
+    cy.contains(/form layouts/i).click();
+
+    //1
+    cy.get('[for="exampleInputEmail1"]').should("contain", "Email address");
+
+    //2
+    cy.get('[for="exampleInputEmail1"]').then((label) => {
+      expect(label.text()).to.equal("Email address");
+    });
+
+    //3
+    cy.get('[for="exampleInputEmail1"]')
+      .invoke("text")
+      .then((text) => {
+        expect(text).to.equal("Email address");
+      });
+
+    cy.contains("nb-card", "Basic form")
+      .find("nb-checkbox")
+      .click()
+      .find(".custom-checkbox")
+      .invoke("attr", "class")
+      //.should("contain", "checked");
+      .then((classValue) => {
+        expect(classValue).to.contain("checked");
+      });
+  });
+
+  it.only("assert property", () => {
+    const selectDayFromCurrent = (day) => {
+      let date = new Date();
+      date.setDate(date.getDate() + day);
+      let futureDay = date.getDate();
+      let futureMonth = date.toLocaleString("default", { month: "short" });
+      let dateAssert = `${futureMonth} ${futureDay}, ${date.getFullYear()}`;
+
+      cy.get("nb-calendar-navigation")
+        .invoke("attr", "ng-reflect-date")
+        .then((dateAttribute) => {
+          if (!dateAttribute.includes(futureMonth)) {
+            cy.get('[data-name="chevron-right"]').click();
+            selectDayFromCurrent(day);
+          } else {
+            cy.get('nb-calendar-day-picker [class="day-cell ng-star-inserted"]')
+              .contains(futureDay)
+              .click();
+          }
+        });
+
+      return dateAssert;
+    };
+
+    cy.visit("/");
+    cy.contains(/forms/i).click();
+    cy.contains(/datepicker/i).click();
+
+    cy.contains("nb-card", "Common Datepicker")
+      .find("input")
+      .then((input) => {
+        cy.wrap(input).click();
+        let dateAssert = selectDayFromCurrent(15);
+
+        cy.wrap(input).invoke("prop", "value").should("contain", dateAssert);
+      });
+  });
+
+  it("radio button", () => {
+    cy.visit("/");
+    cy.contains(/forms/i).click();
+    cy.contains(/form layout/i).click();
+
+    cy.contains("nb-card", "Using the Grid")
+      .find('[type="radio"]')
+      .then((radioButtons) => {
+        cy.wrap(radioButtons)
+          .first()
+          .check({ force: true })
+          .should("be.checked");
+
+        cy.wrap(radioButtons).eq(1).check({ force: true });
+
+        cy.wrap(radioButtons).eq(0).should("not.be.checked");
+
+        cy.wrap(radioButtons).eq(2).should("be.disabled");
+      });
+  });
+
+  it("check boxes", () => {
+    cy.visit("/");
+    cy.contains(/modal & overlays/i).click();
+    cy.contains(/toastr/i).click();
+
+    cy.get('[type="checkbox"]').check({ force: true });
+    cy.get('[type="checkbox"]').eq(0).click({ force: true });
+    cy.get('[type="checkbox"]').eq(1).click({ force: true });
+  });
+
+  it("lists and dropdowns", () => {
+    cy.visit("/");
+
+    //1
+    // cy.get("nav nb-select").click();
+    // cy.get(".options-list").contains(/dark/i).click();
+
+    // cy.get("nav nb-select").should("contain", "Dark");
+    // cy.get("nb-layout-header nav").should(
+    //   "have.css",
+    //   "background-color",
+    //   "rgb(34, 43, 69)"
+
+    //2
+    cy.get("nav nb-select").then((dropdown) => {
+      cy.wrap(dropdown).click();
+      cy.get(".options-list nb-option").each((listItem, index) => {
+        const itemText = listItem.text().trim();
+        const colors = {
+          Light: "rgb(255, 255, 255)",
+          Dark: "rgb(34, 43, 69)",
+          Cosmic: "rgb(50, 50, 89)",
+          Corporate: "rgb(255, 255, 255)",
+        };
+
+        cy.wrap(listItem).click();
+        cy.wrap(dropdown).should("contain", itemText);
+
+        cy.get("nb-layout-header nav").should(
+          "have.css",
+          "background-color",
+          colors[itemText]
+        );
+
+        index < 3 && cy.wrap(dropdown).click();
+      });
+    });
+  });
+
+  it("Web tables", () => {
+    cy.visit("/");
+    cy.contains(/tables & data/i).click();
+    cy.contains(/smart table/i).click();
+
+    //1
+    cy.get("tbody")
+      .contains("tr", "Larry")
+      .then((tableRow) => {
+        cy.wrap(tableRow).find(".nb-edit").click();
+        cy.wrap(tableRow).find('[placeholder="Age"]').clear().type("25");
+        cy.wrap(tableRow).find(".nb-checkmark").click();
+        cy.wrap(tableRow).find("td").eq(6).should("contain", "25");
+      });
+
+    //2
+    cy.get("thead").find(".nb-plus").click();
+    cy.get("thead")
+      .find("tr")
+      .eq(2)
+      .then((tableRow) => {
+        cy.wrap(tableRow)
+          .find('[placeholder="First Name"]')
+          .clear()
+          .type("Artem");
+        cy.wrap(tableRow)
+          .find('[placeholder="Last Name"]')
+          .clear()
+          .type("Bondar");
+        cy.wrap(tableRow).find(".nb-checkmark").click();
+      });
+
+    cy.get("tbody tr")
+      .first()
+      .find("td")
+      .then((tableColumns) => {
+        cy.wrap(tableColumns).eq(2).should("contain", "Artem");
+        cy.wrap(tableColumns).eq(3).should("contain", "Bondar");
+      });
+
+    //3
+    const age = [20, 30, 40, 200];
+
+    cy.wrap(age).each((age) => {
+      cy.get('thead [placeholder="Age"]').clear().type(age);
+      cy.wait(500);
+      // cy.get("tbody tr").should((tableRows) => {
+      //   expect(tableRows).to.contain(age);
+      // });
+
+      cy.get("tbody tr").each((tableRow) => {
+        if (age == 200) {
+          cy.wrap(tableRow).should("contain", "No data found");
+        } else {
+          cy.wrap(tableRow).find("td").eq(6).should("contain", age);
+        }
+      });
+    });
+  });
+});
